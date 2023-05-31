df0bdd6dfc578fc2249f991b29a58485395e11d5
shohag <pkshohag240@gmail.com>
2023-05-31 15:03:29 +0600

    customer login page added

--- pages/login/index.js
+++ pages/login/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import CustomerLayout from "../../layouts/customerLayout";
 
-const SignUp = () => {
+const CustomerLogin = () => {
   const { handleSubmit, control } = useForm({
     defaultValues: {
       email: "",
@@ -21,7 +21,7 @@ const SignUp = () => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Sign Up</title>
+        <title>Login</title>
       </Head>
       <main>
         <div className="h-screen w-full flex border-b-2 bg-gray-100">
@@ -39,15 +39,15 @@ const SignUp = () => {
                     <div className="space-y-2">
                       <div>
                         <h1 className="text-xl font-medium text-center md:text-2xl font-roboto">
-                          Welcome to HarriShop!
+                          Welcome Back!
                         </h1>
                       </div>
                       <div>
                         <div className="space-x-1 text-sm text-center md:text-base font-nunito">
-                          <span>Already have and Account?</span>
-                          <Link href="/login">
+                          <span>New to HarriShop?</span>
+                          <Link href="/signup">
                             <button className="font-semibold text-blue-500">
-                              Login
+                              SignUp
                             </button>
                           </Link>
                         </div>
@@ -102,31 +102,6 @@ const SignUp = () => {
                               required
                             />
                           </div>
-
-                          <div className="relative flex items-center">
-                            <svg
-                              className="absolute w-5 h-5 ml-3 text-gray-400 "
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                              />
-                            </svg>
-                            <input
-                              className="w-full p-2 pl-10 text-gray-800 placeholder-gray-600 rounded-md  border focus:outline-none focus:ring-2 focus:ring-blue-300"
-                              type="password"
-                              name="password"
-                              placeholder="Confirm Password"
-                              required
-                            />
-                          </div>
-
                           <div className="flex items-start space-x-2 md:items-center">
                             <input
                               className="focus:outline-none"
@@ -141,10 +116,9 @@ const SignUp = () => {
                               <span>Remember Me</span>
                             </label>
                           </div>
-
                           <div>
                             <button className="w-full p-2 text-sm font-semibold text-center text-white transition duration-100 rounded-md md:text-lg font-nunito bg-gradient-to-r from-blue-600 to-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-300 hover:shadow-lg">
-                              Sign Up
+                              Sign In
                             </button>
                           </div>
                         </div>
@@ -177,8 +151,8 @@ const SignUp = () => {
   );
 };
 
-SignUp.getLayout = (page) => {
+CustomerLogin.getLayout = (page) => {
   return <CustomerLayout>{page}</CustomerLayout>;
 };
 
-export default SignUp;
+export default CustomerLogin;
