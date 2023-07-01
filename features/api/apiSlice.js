7821813f20b384a0021d7a0f644b0e341e189578
shohag roy <pkshohag240@gmail.com>
2023-07-02 02:53:05 +0600

    google auth redirect url code updated

--- features/api/apiSlice.js
+++ features/api/apiSlice.js
@@ -3,9 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_DEVELOPMENT
-      ? "http://localhost:5000/api/v1"
-      : process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+    baseUrl:
+      process.env.NEXT_PUBLIC_DEVELOPMENT === "development"
+        ? "http://localhost:5000/api/v1"
+        : process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
 
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
